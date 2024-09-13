793210a85dc1a5305dae01f85fa93faa30789612
safinelahi <safinelahi@gmail.com>
2024-09-14 04:50:33 +0600

    triangle and rectangle are calculation and disply area in a area display section

--- js/triangleCalcu.js
+++ js/triangleCalcu.js
@@ -1,3 +1,6 @@
+//triangle
+// global triangle value 
+let triangleArea = 0
 function triangleCalculate(){
     //console.log('done')
     // triangle base value
@@ -12,4 +15,52 @@ function triangleCalculate(){
     const triangleHeightInputValue= triangleHeightInput.value
     const triangleHeight = parseFloat(triangleHeightInputValue)
     console.log(triangleHeight)
+    // calculation triangle area
+     triangleArea = 0.5 * triangleBase * triangleHeight
+    //console.log(triangleArea)
+    // display triangle are 
+    const displayTriangleArea = document.getElementById('display-triangle-area')
+    //console.log(displayTriangleArea)
+    displayTriangleArea.innerText = triangleArea
 }
+// convert cm to m in triangle area
+document.getElementById('convert-triangle-area').addEventListener('click',function(){
+    //console.log(triangleArea)
+    const convertTriangleArea = triangleArea / 100
+    const displayConvertTriangleArea = document.getElementById('display-triangle-area')
+    displayConvertTriangleArea.innerText = convertTriangleArea
+})
+
+// Rectangle
+//global Rectangle area 
+let rectangleArea = 0
+
+function rectangleCalculation(){
+    //console.log('hello')
+    // rectangle Wight value
+    const rectangleWightInput = document.getElementById('rectangle-wight')
+    const rectangleWightValue = rectangleWightInput.value
+    //console.log(rectangleWightValue)
+    const rectangleWight = parseInt(rectangleWightValue)
+    console.log(rectangleWight)
+
+    // rectangle length value
+    const rectangleLengthInput = document.getElementById('rectangle-length')
+    const rectangleLengthValue = rectangleLengthInput.value
+    const rectangleLength = parseInt(rectangleLengthValue)
+    console.log(rectangleLength)
+
+    // Calculation rectangle Area 
+    rectangleArea = rectangleWight * rectangleLength
+    console.log(rectangleArea)
+
+    // display rectangle area 
+    const displayRectangleArea = document.getElementById('display-rectangle-area')
+    displayRectangleArea.innerText = rectangleArea
+}
+
+// convert cm to m in rectangle-area
+document.getElementById('convert-rectangle-area').addEventListener('click', function(){
+    const convertRectangleArea= document.getElementById('display-rectangle-area')
+    convertRectangleArea.innerText= rectangleArea / 100
+})
